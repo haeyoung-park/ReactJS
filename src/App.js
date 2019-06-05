17cf8553dea816c97eb261acf7af266ca55f4723
boredash <borebash@gmail.com>
2019-06-05 15:48:03 +0900

    Finish
    2019.06.05

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React, {Component} from 'react';
 import './App.css';
 import Movie from './Movie';
-import { throwStatement } from '@babel/types';
 
 
 class App extends Component{
@@ -10,48 +9,55 @@ class App extends Component{
     
   // Update componentWillReceiveProps () => shouldComponentUpdate () == true => componentWillUpdate () => render () => componentDidMount ()
 
+
+
   // State가 변경될 때 마다 Component는 다시 Render하게 된다.
-  state = {
-  } 
+
+  // Component -  State              :            - Class
+  //           ㄴ Statless Component : only Props - function(Props.value (ex. poster))  : 그냥 return을 위해 존재   don't have state! 
+  
+  state = {}
+
   componentDidMount (){
-    setTimeout(()=> {
-      this.setState({
-          movies : [
-            {
-              title  : 'BabaraPalvin',
-              poster : 'https://nation.com.pk/print_images/medium/2019-03-15/barbara-palvin-named-victoria-s-secret-angel-1552670925-8836.jpg'
-            },
-            {
-              title  : 'WHO YOU ARE',
-              poster : 'https://img-s-msn-com.akamaized.net/tenant/amp/entityid/BBUJuVA.img?h=195&w=270&m=6&q=60&o=f&l=f&x=215&y=149'
-            },
-            {
-              title  : 'H',
-              poster : 'https://s27589.pcdn.co/wp-content/uploads/2018/03/Katniss.jpg'
-            },
-            {
-              title  : 'IronMan ',
-              poster : 'https://upload.wikimedia.org/wikipedia/en/d/d5/Iron_Man_3_theatrical_poster.jpg'
-            },
-            {
-              title  : 'IronMan2',
-              poster : 'https://i2.wp.com/thisisdotun.com/wp-content/uploads/2018/05/marvel-avengers-infinity-war-iron-man-sixth-scale-figure-hot-toys-feature-903421.jpg?resize=768%2C465&ssl=1'
-            }
-          ]
-        })
-      }, 5000
-    )
+    this._getMovies();
   }
 
   _renderMovies = () => {
-    const movies = [<Movie props/>, <Movie props/>] ;
+    const movies = this.state.movies.map((movie) => {
+      
+      return <Movie 
+          title = {movie.title} 
+          poster = {movie.medium_cover_image} 
+          key = {movie.id}
+          genres = {movie.genres}
+          synopsis = {movie.synopsis}  
+        />
+    })
      return movies;
   }
 
+   _getMovies = async () => {
+    const movies = await this._callApi();
+    this.setState({
+      movies
+    })
+  }
+
+  _callApi = () => {
+    // console.log(fetch('https://yts.am/api/v2/list_movies.json?sort_by-rating')); // Promise : new JavaScript Concept <- Asynchronous
+
+    return fetch('https://yts.am/api/v2/list_movies.json?sort_by=like_count') // Ajax
+
+    .then(response => response.json())
+    .then(json => json.data.movies)
+    .catch(err => console.log(err))
+  }
+
   render(){
+    const { movies } = this.state;
     return (
-      <div className="App">
-          {this.state.movies ? this._renderMovies() : <h1>Loading</h1>}
+      <div className = {movies ? "App" : "App--loading"}>
+          {movies ? this._renderMovies() : 'Loading'}
       </div>
     );
   }
--- src/Movie.js
+++ src/Movie.js
@@ -1,39 +1,60 @@
-import React, {Component} from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import './Movie.css';
+import LineEllipsis from 'react-lines-ellipsis';
 
-
-class Movie extends Component{
-
-    static propTypes = {
-        title  : PropTypes.string.isRequired,
-        poster : PropTypes.string.isRequired
-    }
-
-    render(){
-        return(
-            <div>
-                <MoviePoster poster = {this.props.poster}/>
-                <h1>
-                    {this.props.title}
-                </h1>
-                
+    function Movie ({title, poster, genres, synopsis}){
+        return (
+            <div className = "Movie">
+                <div className = "Movie_Column">
+                    <MoviePoster poster = {poster} alt={title}/>
+                </div>
+                <div className = "Movie_Column">
+                    <h1>{title}</h1>
+                    <div className = "Movie_Genres">
+                        {genres.map((genre, index) => <MovieGenre genre={genre} key={index}/>)}                        
+                    </div>
+                    <p className = "Movie_Synopsis">
+                        <LineEllipsis
+                            text = {synopsis}
+                            maxLine = '3'
+                            ellipsis = '...'
+                            trimRight
+                            baseOn = 'letters'
+                        />
+                    </p>
+                </div>
             </div>
         );
     }
-} 
 
-class MoviePoster extends Component{
-
-    static propTypes = {
-        poster : PropTypes.string.isRequired
+    function MoviePoster({poster, alt}){
+            return(
+                <img className = "Movie_Poster" src={poster} alt={alt} title = {alt}/>
+            )
     }
 
-    render(){
+    function MovieGenre({genre}){
         return(
-            <img src={this.props.poster}></img>
-        );
+            <span className = "Movie_Genre">{genre}</span>
+        )
     }
-}
+
+    Movie.propsTypes = {
+        title    : PropTypes.string.isRequired,
+        poster   : PropTypes.string.isRequired,
+        genres   : PropTypes.string.isRequired,
+        synopsis : PropTypes.string.isRequired
+    }
+    
+    MoviePoster.propsTypes = {
+        poster : PropTypes.string.isRequired, 
+        alt    : PropTypes.string.isRequired
+    }
+
+    MovieGenre.propsTypes = {
+        genre : PropTypes.string.isRequired
+    }
+
 
 export default Movie;
